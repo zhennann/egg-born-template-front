86fed4bb43661c0c642a514bcc5ed26a79088ae7
zhennann <zhennann@qq.com>
2017-10-13 00:20:43 +0800

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+* @Author: zhennann
+* @Date:   2017-10-12 16:37:55
+* @Last Modified by:   zhennann
+* @Last Modified time: 2017-10-13 00:10:57
+*/
+
+module.exports = {
+  name: {
+    desc: 'project name',
+  },
+  description: {
+    desc: 'project description',
+  },
+  author: {
+    desc: 'project author',
+  },
+};
