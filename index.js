472ce8df49afb1819a215a1adb9904a451b8b774
zhennann <zhen.nann@icloud.com>
2017-11-28 10:29:54 +0800

    refactor: eslint

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/*
-* @Author: zhennann
-* @Date:   2017-10-12 16:37:55
-* @Last Modified by:   zhennann
-* @Last Modified time: 2017-10-13 00:10:57
-*/
-
 module.exports = {
   name: {
     desc: 'project name',
